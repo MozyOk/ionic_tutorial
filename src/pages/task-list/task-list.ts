cdbc3bab03e250cacbde6f26db86ba56f262c48b
MozyOk <mozy.okubo@gmail.com>
2018-02-04 19:39:02 +0900

    add: タスク一覧で削除、変更、閉じるのアクションシートを表示できるようにした p82

--- src/pages/task-list/task-list.ts
+++ src/pages/task-list/task-list.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ActionSheetController } from 'ionic-angular';
 
 /**
  * Generated class for the TaskListPage page.
@@ -14,18 +14,48 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'task-list.html',
 })
 export class TaskListPage {
-  tasks: { name: string }[] =[
-    {name: 'タスク１'},
-    {name: 'タスク２'}
+  tasks: { name: string }[] = [
+    { name: 'タスク１' },
+    { name: 'タスク２' }
   ];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public actionSheetCtrl: ActionSheetController) {
   }
 
   ionViewWillEnter() {
-   if(localStorage.getItem('tasks')){
-     this.tasks = JSON.parse(localStorage.getItem('tasks'));
-   }
+    if (localStorage.getItem('tasks')) {
+      this.tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+  }
+
+  changeTask() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'タスクの変更',
+      buttons: [
+        {
+          text: '削除',
+          role: 'destructive',
+          handler: () => {
+            console.log('Destructive clicked');
+          }
+        }, {
+          text: '変更',
+          handler: () => {
+            console.log('Archive clicked');
+          }
+        }, {
+          text: '閉じる',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
   }
 
 }
